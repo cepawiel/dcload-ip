b01bb5314cfa628b70c8558c9cbd57605bf98d62
cepawiel <cepawiel@mtu.edu>
2022-12-11 07:32:28 -0600

    Cleanup Meson

--- target/build.sh
+++ target/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+BUILD_DIR=builddir
 SH4_CROSS_FILE=sh4-kos-gcc.txt
 
 if [[ $1 == docker ]]; then
-    docker run -v $(pwd):/src --rm einsteinx2/dcdev-gcc-toolchain:latest "apk add meson && ./build.sh"
+    docker run -v $(pwd):/src --rm einsteinx2/dcdev-gcc-toolchain:latest "apk add binutils meson && ./build.sh"
 else
-    meson setup --cross-file $SH4_CROSS_FILE builddir && \
-    meson compile -C builddir
+    meson setup --cross-file $SH4_CROSS_FILE $BUILD_DIR && \
+    meson compile -C $BUILD_DIR --verbose && \
+    readelf --sections $BUILD_DIR/dcload/dcloadip.elf
 fi
 exit $?

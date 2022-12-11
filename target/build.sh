daaea1b28639499faea2ad56431037554162d627
cepawiel <cepawiel@mtu.edu>
2022-12-10 19:20:18 -0600

    Build CDI with Github Actions

--- target/build.sh
+++ target/build.sh
@@ -5,7 +5,7 @@ SH4_CROSS_FILE=sh4-kos-gcc.txt
 if [[ $1 == docker ]]; then
     docker run -v $(pwd):/src --rm einsteinx2/dcdev-gcc-toolchain:latest "apk add meson && ./build.sh"
 else
-    meson setup --cross-file $SH4_CROSS_FILE build && \
-    meson compile -C build
+    meson setup --cross-file $SH4_CROSS_FILE builddir && \
+    meson compile -C builddir
 fi
 exit $?
